15e404751ed11d5bc1ca29d4829ee0a0bdf702c1
Radifire <Jaghoghov@gmail.com>
2024-03-08 14:16:22 +0000

    final touchoups

--- api/services.py
+++ api/services.py
@@ -5,29 +5,34 @@ from datetime import datetime
 
 async def get_available_appointments(db: AsyncSession) -> list[schemas.AvailabilityBase]:
     async with db as session:
-        result = await session.execute(select(models.Availability).where(models.Availability.start_time > datetime.today()))
+        result = await session.execute(select(models.Availability)
+                                       .where(models.Availability.start_time > datetime.today())
+                                       .where(models.Availability.status == models.Status.available))
         return result.scalars().all()
 
 # get available appointments for a healthcare provider after the current time
 async def get_provider_available_appointments(provider_id: str, db: AsyncSession) -> list[schemas.AvailabilityBase]:
     async with db as session:
-        result = await session.execute(select(models.Availability).where(models.Availability.provider_id == provider_id).where(models.Availability.status == models.Status.available).where(models.Availability.start_time > datetime.today()))
+        result = await session.execute(select(models.Availability)
+                                       .where(models.Availability.provider_id == provider_id)
+                                       .where(models.Availability.status == models.Status.available)
+                                       .where(models.Availability.start_time > datetime.today()))
         return result.scalars().all()
 # get patient's upcoming appointments after the current time
 async def get_patient_upcoming_appointments(patient_id: str, db: AsyncSession) -> list[schemas.AppointmentBase]:
     async with db as session:
-        result = await session.execute(select(models.Appointment).where(models.Appointment.patient_id == patient_id).where(models.Appointment.end_time > datetime.now()))
+        result = await session.execute(select(models.Appointment)
+                                       .where(models.Appointment.patient_id == patient_id)
+                                       .where(models.Appointment.end_time > datetime.now()))
         return result.scalars().all()
     
-async def get_provider_upcoming_appointments(provider_id: str, db: AsyncSession) -> list[schemas.AppointmentBase]:
-    async with db as session:
-        result = await session.execute(select(models.Appointment).where(models.Appointment.provider_id == provider_id).where(models.Appointment.end_time > datetime.now()))
-        return result.scalars().all()
 
 # get upcoming appointments for a healthcare provider after the current time
 async def get_provider_scheduled_appointments(provider_id: str, db: AsyncSession) -> list[schemas.AppointmentBase]:
     async with db as session:
-        result = await session.execute(select(models.Appointment).where(models.Appointment.provider_id == provider_id).where(models.Appointment.end_time > datetime.now()))
+        result = await session.execute(select(models.Appointment)
+                                       .where(models.Appointment.provider_id == provider_id)
+                                       .where(models.Appointment.end_time > datetime.now()))
         return result.scalars().all()
 
 
@@ -70,30 +75,33 @@ async def book_appointment(appointment: schemas.AppointmentCreate, db: AsyncSess
 
 # cancel an appointment
 async def cancel_appointment(appointment_id: int, db: AsyncSession):
-    async with db as session:
-        query = await session.execute(select(models.Appointment).where(models.Appointment.id == appointment_id))
+    async with db.begin():
+        query = await db.execute(select(models.Appointment).where(models.Appointment.id == appointment_id))
         appointment = query.scalars().first()
-        if appointment.status != models.AppointmentStatus.scheduled:
+        if appointment is None or appointment.status != models.AppointmentStatus.scheduled:
             raise ValueError("Appointment cannot be cancelled")
         appointment.status = models.AppointmentStatus.cancelled
-        session.commit()
         # update availability status for healthcare provider
-        query = await session.execute(select(models.Availability).where(models.Availability.provider_id == appointment.provider_id).where(models.Availability.start_time == appointment.scheduled_time))
+        query = await db.execute(select(models.Availability)
+                                 .where(models.Availability.provider_id == appointment.provider_id)
+                                 .where(models.Availability.start_time == appointment.scheduled_time))
         availability = query.scalars().first()
         availability.status = models.Status.available
-        session.commit()
+        await db.commit()
         return {"message": "Appointment cancelled successfully"}
 
 # get patient's appointments
 async def get_patient_appointments(patient_id: str, db: AsyncSession) -> list[schemas.Appointment]:
     async with db as session:
-        result = await session.execute(select(models.Appointment).where(models.Appointment.patient_id == patient_id))
+        result = await session.execute(select(models.Appointment)
+                                       .where(models.Appointment.patient_id == patient_id))
         return result.scalars().all()
 
 # get healthcare provider's appointments
 async def get_provider_appointments(provider_id: str, db: AsyncSession) -> list[schemas.Appointment]:
     async with db as session:
-        query = await session.execute(select(models.Appointment).where(models.Appointment.provider_id == provider_id))
+        query = await session.execute(select(models.Appointment)
+                                      .where(models.Appointment.provider_id == provider_id))
         return query.scalars().all()
 
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -20,8 +20,8 @@ async def test_view_available_appointments():
     # Import the FastAPI app after setting up the test environment fixture
     from api.main import app
     mock_return_value = [
-        { "provider_id": 1, "start_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Available"},
-        { "provider_id": 1, "start_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T10:30:00", "status": "Available"}
+        { "provider_id": "1", "start_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Available"},
+        { "provider_id": "1", "start_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T10:30:00", "status": "Available"}
     ]
 
     with patch('api.services.get_available_appointments', return_value=mock_return_value):
@@ -40,8 +40,8 @@ async def test_view_patient_upcoming_appointments():
     from api.main import app
 
     mock_return_value = [
-        {"patient_id": 1 , "provider_id": 1, "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup" , "video_link": "https://example.com/video/2022-01-01T09:00:00"},
-        {"patient_id": 1 , "provider_id": 2, "scheduled_time": "2022-02-01T10:00:00", "end_time": "2022-02-01T10:30:00", "status": "Scheduled", "reason_for_visit": "Follow Up" , "video_link": "https://example.com/video/2022-01-01T10:00:00"}
+        {"patient_id": "1" , "provider_id": "1", "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00",  "reason_for_visit": "Annual checkup" },
+        {"patient_id": "1" , "provider_id": "2", "scheduled_time": "2022-02-01T10:00:00", "end_time": "2022-02-01T10:30:00",  "reason_for_visit": "Follow Up" }
     ]
 
     with patch('api.services.get_patient_upcoming_appointments', return_value=mock_return_value):
@@ -52,19 +52,22 @@ async def test_view_patient_upcoming_appointments():
                 assert response.status_code == 200
                 assert response.json() == mock_return_value
 
+# view my upcoming appointments
 @pytest.mark.asyncio
 async def test_view_provider_upcoming_appointments():
     # Import the FastAPI app after setting up the test environment fixture
     from api.main import app
+
     mock_return_value = [
-        {"patient_id": 1 , "provider_id": 1, "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup" , "video_link": "https://example.com/video/2022-01-01T09:00:00"},
-        {"patient_id": 2 , "provider_id": 1, "scheduled_time": "2022-02-01T10:00:00", "end_time": "2022-02-01T10:30:00", "status": "Scheduled", "reason_for_visit": "Follow Up" , "video_link": "https://example.com/video/2022-01-01T10:00:00"}
+        {"patient_id": "1" , "provider_id": "1", "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00",  "reason_for_visit": "Annual checkup" },
+        {"patient_id": "2" , "provider_id": "1", "scheduled_time": "2022-02-01T10:00:00", "end_time": "2022-02-01T10:30:00",  "reason_for_visit": "Follow Up" }
     ]
 
-    with patch('api.services.get_provider_upcoming_appointments', return_value=mock_return_value):
+    with patch('api.services.get_provider_scheduled_appointments', return_value=mock_return_value):
         async with LifespanManager(app):
+            # async with AsyncClient(app=app, base_url="http://test") as client:
             async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
-                response = await client.get("/appointments/1/upcoming")
+                response = await client.get("/appointments/1/scheduled")
                 assert response.status_code == 200
                 assert response.json() == mock_return_value
 
@@ -73,13 +76,13 @@ async def test_view_provider_upcoming_appointments():
 async def test_book_appointment():
     # Import the FastAPI app after setting up the test environment fixture
     from api.main import app
-    mock_return_value = {"id": 1,"patient_id": 1, "provider_id": 1, "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup", "video_link": "https://example.com/video/2022-01-01T09:00:00"}
+    mock_return_value = {"id": 1,"patient_id": "1", "provider_id": "1", "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup", "video_link": "https://example.com/video/2022-01-01T09:00:00"}
 
     with patch('api.services.book_appointment', return_value=mock_return_value):
         async with LifespanManager(app):
             # async with AsyncClient(app=app, base_url="http://test") as client:
             async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
-                response = await client.post("/appointments/book", json={"patient_id": 1, "provider_id": 1, "start_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "reason_for_visit": "Annual checkup"})
+                response = await client.post("/appointments/book", json={"patient_id": "1", "provider_id": "1", "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "reason_for_visit": "Annual checkup"})
                 assert response.status_code == 200, response.json()
                 assert response.json() == mock_return_value
 
@@ -103,8 +106,8 @@ async def test_view_patient_appointments():
     # Import the FastAPI app after setting up the test environment fixture
     from api.main import app
     mock_return_value = [
-        {"id": 1, "patient_id": 1, "provider_id": 1, "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup", "video_link": "https://example.com/video/2022-01-01T09:00:00"},
-        {"id": 2, "patient_id": 1, "provider_id": 2, "scheduled_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T10:30:00", "status": "Scheduled", "reason_for_visit": "Follow Up", "video_link": "https://example.com/video/2022-01-01T10:00:00"}
+        {"id": 1, "patient_id": "1", "provider_id": "1", "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup", "video_link": "https://example.com/video/2022-01-01T09:00:00"},
+        {"id": 2, "patient_id": "1", "provider_id": "2", "scheduled_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T10:30:00", "status": "Scheduled", "reason_for_visit": "Follow Up", "video_link": "https://example.com/video/2022-01-01T10:00:00"}
     ]
 
     with patch('api.services.get_patient_appointments', return_value=mock_return_value):
@@ -120,8 +123,8 @@ async def test_view_provider_appointments():
     # Import the FastAPI app after setting up the test environment fixture
     from api.main import app
     mock_return_value = [
-        {"id": 1, "patient_id": 1, "provider_id": 1, "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup", "video_link": "https://example.com/video/2022-01-01T09:00:00"},
-        {"id": 2, "patient_id": 2, "provider_id": 1, "scheduled_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T10:30:00", "status": "Scheduled", "reason_for_visit": "Follow Up", "video_link": "https://example.com/video/2022-01-01T10:00:00"}
+        {"id": 1, "patient_id": "1", "provider_id": "1", "scheduled_time": "2022-01-01T09:00:00", "end_time": "2022-01-01T09:30:00", "status": "Scheduled", "reason_for_visit": "Annual checkup", "video_link": "https://example.com/video/2022-01-01T09:00:00"},
+        {"id": 2, "patient_id": "2", "provider_id": "1", "scheduled_time": "2022-01-01T10:00:00", "end_time": "2022-01-01T10:30:00", "status": "Scheduled", "reason_for_visit": "Follow Up", "video_link": "https://example.com/video/2022-01-01T10:00:00"}
     ]
 
     with patch('api.services.get_provider_appointments', return_value=mock_return_value):
